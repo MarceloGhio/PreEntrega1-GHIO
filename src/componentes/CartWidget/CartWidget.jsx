0c089198a6518424ee1055b302734ce210a2480c
Marce Ghio <Marce.ghio.mg@gmail.com>
2023-06-10 23:29:30 -0300

    PreEntrega1-Ghio

---
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+        <img style={{width: "4rem"}} src="./img/carritoBlanco.jpg" alt="Carrito de compras" />
+        <strong style={{color: "white"}}> 10 </strong>
+    </div>
+  )
+}
+
+export default CartWidget
