f16254313bc7a4e422680d92682a2f014032d640
Marce Ghio <Marce.ghio.mg@gmail.com>
2023-07-14 14:31:26 -0300

    ItemCount clase-10

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const misProductos = [
-    {id:"1", nombre: "Madara", precio: 10000, img: "../public/img/madara.jpg", idCat: "uchiha"},
-    {id:"2", nombre: "Tobi", precio: 9500, img: "../public/img/tobi.PNG", idCat: "uchiha"},
-    {id:"3", nombre: "Itachi", precio: 9000, img: "../public/img/itachi.PNG", idCat: "uchiha"},
-    {id:"4", nombre: "Pain", precio: 8000, img: "../public/img/pain.jpg", idCat: "uzumaki"},
+    {id:"1", nombre: "Madara", precio: 10000, stock: 10, img: "../public/img/madara.jpg", idCat: "uchiha"},
+    {id:"2", nombre: "Tobi", precio: 9500, stock: 10, img: "../public/img/tobi.PNG", idCat: "uchiha"},
+    {id:"3", nombre: "Itachi", precio: 9000, stock: 10, img: "../public/img/itachi.PNG", idCat: "uchiha"},
+    {id:"4", nombre: "Pain", precio: 8000, stock: 20, img: "../public/img/pain.jpg", idCat: "uzumaki"},
 ]
 
 export const getProductos = () => {
